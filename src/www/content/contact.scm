71a82c1d029d40d05972a9b5fcb40c30ee5f7178
Steven Wiley <s.wiley@katchitek.com>
2021-06-03 07:08:47 +0900

    Removed phone number and mailing address from 'Contact' page. Added redirects for canonical URIs.

--- src/www/content/contact.scm
+++ src/www/content/contact.scm
@@ -1,26 +1,9 @@
 `(section ,(section-title "Contact")
 
-	  (main ;; style: "width:255px;"
-
-		
-
-		(h2 "Email")
-		(p "contact@s-expressions.org")
-		(br)
-		(h2 "Telephone")
-		(p "+1 (760) 760-7151")
-		(br) 
-		(h2 "Mailing Address")
-		(p "The S-expressions organization" (br)
-		   (br)
-		   "c/o Steven Wiley" (br)
-		   "5348 Vegas Drive" (br)
-		   "Las Vegas, Nevada 89108" (br)
-		   "USA")
-		)
-
-	  (aside ;; style: "width:475px"
-
+	  (main  (br)
+		 (h2 "Email")
+	  	 (p "contact@s-expressions.org")
+		 (br)
 		 (h2 "GitHub")
 		 (p (a href: "https://github.com/s-expressions" "https://github.com/s-expressions"))
 		 (br)
@@ -29,8 +12,7 @@
 		 (br)
 		 (h2 "IRC")
 		 (p "liberachat: #s-expressions")
-		 
 
 		 )
-
 	  )
+	 
